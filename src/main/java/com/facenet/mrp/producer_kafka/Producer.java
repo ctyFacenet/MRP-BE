cdfc08a8bd7fdd9657bda51aada5a31493112da8
Nguyễn Quang Hiếu <nguyenquanghieu2000d@gmail.com>
2024-04-08 15:50:54 +0700

    update
    update

--- src/main/java/com/facenet/mrp/producer_kafka/Producer.java
+++ src/main/java/com/facenet/mrp/producer_kafka/Producer.java
@@ -24,7 +24,7 @@ public class Producer {
     /**
      * cảnh báo đơn hàng so và fc
      */
-    @Scheduled(cron = "0 0 8 * * ?")
+    @Scheduled(cron = "0 0 8,16 * * ?")
     public void sendMessage(){
         Gson gson = new Gson();
         String topic = "mrp-so";
@@ -39,7 +39,7 @@ public class Producer {
     /**
      * cảnh báo nvl,btp trong đơn hàng so và fc
      */
-    @Scheduled(cron = "0 0 8 * * ?")
+    @Scheduled(cron = "0 0 8,16 * * ?")
     public void sendMessageForItem(){
         Gson gson = new Gson();
         String topic = "mrp-so-item";
