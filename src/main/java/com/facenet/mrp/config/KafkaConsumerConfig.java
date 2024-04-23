3e7d97bd9e18d73d9528b1859f5424a793b1ca72
Trần đình thành <65401479+ThanhTDHaTinh2000@users.noreply.github.com>
2024-04-23 14:21:02 +0700

    Update KafkaConsumerConfig.java

--- src/main/java/com/facenet/mrp/config/KafkaConsumerConfig.java
+++ src/main/java/com/facenet/mrp/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ package com.facenet.mrp.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,6 +39,8 @@ public class KafkaConsumerConfig {
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.68.76:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // Thiết lập max.poll.interval.ms tại đây
         return new DefaultKafkaProducerFactory<>(props);
     }
