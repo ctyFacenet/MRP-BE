460ea0165cac405634da6e19e3a6826fc7022eaa
Nguyễn Quang Hiếu <nguyenquanghieu2000d@gmail.com>
2024-04-23 14:08:45 +0700

    Update KafkaConsumerConfig.java

--- src/main/java/com/facenet/mrp/config/KafkaConsumerConfig.java
+++ src/main/java/com/facenet/mrp/config/KafkaConsumerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -33,10 +34,16 @@ public class KafkaConsumerConfig {
 //    @Value("${spring.kafka.consumer.key-deserializer}")
 //    private String keyDeserializer;
 
-    public ProducerFactory<String, String> consumerFactory() {
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.68.76:9092");
         // Thiết lập max.poll.interval.ms tại đây
         return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
