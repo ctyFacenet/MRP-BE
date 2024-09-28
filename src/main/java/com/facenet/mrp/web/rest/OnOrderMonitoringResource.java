f4ef1780863cd68b5360fe0087e62a2a56b3e3ea
PhiVietHoang <111297358+PhiVietHoang@users.noreply.github.com>
2024-09-28 14:10:09 +0700

    update

--- src/main/java/com/facenet/mrp/web/rest/OnOrderMonitoringResource.java
+++ src/main/java/com/facenet/mrp/web/rest/OnOrderMonitoringResource.java
@@ -200,15 +200,15 @@ public class OnOrderMonitoringResource {
         return result;
     }
 
-    @PostMapping("/to-excel")
-    public ResponseEntity<?> exportToExcel(@RequestBody CreatePurchaseOrderDTO input) {
-        try {
-            return monitoringService.exportToExcel(input);
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred during export: " + e.getMessage());
-        }
-    }
+//    @PostMapping("/to-excel")
+//    public ResponseEntity<?> exportToExcel(@RequestBody CreatePurchaseOrderDTO input) {
+//        try {
+//            return monitoringService.exportToExcel(input);
+//
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred during export: " + e.getMessage());
+//        }
+//    }
 
     @GetMapping(value = "/to-excel-pr", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<byte[]> exportToExcelPr() {
